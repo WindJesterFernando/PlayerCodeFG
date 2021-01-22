624ce7066a155aecd24222adf8f5ee31ff630514
Fernando <linksgreenhat@gmail.com>
2021-01-21 19:51:38 -0500

    battle report added

--- PartyAI.cs
+++ PartyAI.cs
@@ -6,32 +6,29 @@ namespace Final_Gambit_Player_Code
 {
     static public class PartyAI
     {
-        static public void ProcessBlackMageAI()
-        {
-
-        }
 
         static public void ProcessAI()
         {
 
+            //FinalGambit.PerformBattleAction(FinalGambit.BattleActionID.Attack, FinalGambit.BattleState.foeCharacters.Last.Value);
+
+
+
+            foreach (RecordOfBattleAction rba in FinalGambit.recordOfBattleActions)
+                Console.WriteLine(rba.battleActionID + " - " + rba.characterWithInitiative.classID);
+
+
             PartyCharacter target = null;
-            
-            foreach(PartyCharacter pc in FinalGambit.BattleState.foeCharacters)//partyCharacters)//FinalGambit.BattleState.foeCharacters)
+
+            foreach (PartyCharacter pc in FinalGambit.BattleState.foeCharacters)
             {
-                if(target == null)
+                if (target == null)
                     target = pc;
-                else if(pc.hp > target.hp)
+                else if (pc.hp > target.hp)
                     target = pc;
             }
 
-            //Console.WriteLine(FinalGambit.BattleState.foeCharacters.Count);
-
-            FinalGambit.PerformBattleAction(FinalGambit.BattleActionID.Attack, target);//FinalGambit.BattleState.foeCharacters.Last.Value);
-
-
-
-            //FinalGambit.PerformBattleAction(FinalGambit.BattleActionID.PhoenixDown, target);
-            //FinalGambit.PerformBattleAction(FinalGambit.BattleActionID.MegaElixer, target);
+            FinalGambit.PerformBattleAction(FinalGambit.BattleActionID.Attack, target);
 
         }
     }
