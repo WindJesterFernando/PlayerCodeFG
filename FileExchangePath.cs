1f4af0b65018a8a7023319ae11601fbca53d384b
Fernando <linksgreenhat@gmail.com>
2021-02-05 13:14:38 -0500

    comment clean up

--- FileExchangePath.cs
+++ FileExchangePath.cs
@@ -4,8 +4,8 @@ namespace Final_Gambit_Player_Code
 {
     static public class FileExchangePath
     {
+        //...................................Paste your folder path below!
         public const string exchangePath = "/Users/fernandorestituto/Desktop/FGFileExchange2";
-        //public const string exchangePath = "PasteYourFolderPathHere";
     }
 
 }
